17884aded888e9603d0ef05e917af2b7695921bd
urosjarc <jar.fmf@gmail.com>
2022-11-23 14:30:44 +0100

    Testiranje kontakta.

--- test/01_domain_tests/test_arhitektura_kluba.py
+++ test/01_domain_tests/test_arhitektura_kluba.py
@@ -2,10 +2,10 @@ import unittest
 from datetime import datetime, timedelta
 
 from app.db import db_entities
-from core.domain.arhitektura_kluba import TipOsebe
+from core.domain.arhitektura_kluba import TipOsebe, Kontakt, TipKontakta, TipValidacije
 
 
-class test_arhitektura_kluba(unittest.TestCase):
+class test_tip_osebe(unittest.TestCase):
 
 	def test_scopes(self):
 		vals = TipOsebe.values()
@@ -15,17 +15,36 @@ class test_arhitektura_kluba(unittest.TestCase):
 		for sk, sv in scopes.items():
 			self.assertIn(sk, sv)
 
-	def test_clan_starost(self):
+
+class test_kontakt(unittest.TestCase):
+
+	def test_eq(self):
+		k0 = Kontakt(data='1234', tip=TipKontakta.EMAIL, validacija=TipValidacije.NI_VALIDIRAN)
+		kontakti = [
+			Kontakt(data='1234', tip=TipKontakta.EMAIL, validacija=TipValidacije.VALIDIRAN),
+			Kontakt(data='1234', tip=TipKontakta.EMAIL, validacija=TipValidacije.POTRJEN),
+			Kontakt(data='1234', tip=TipKontakta.PHONE, validacija=TipValidacije.NI_VALIDIRAN),
+			Kontakt(data='1234', tip=TipKontakta.PHONE, validacija=TipValidacije.VALIDIRAN),
+			Kontakt(data='1234', tip=TipKontakta.PHONE, validacija=TipValidacije.POTRJEN)
+		]
+		for k in kontakti:
+			self.assertEqual(k0, k)
+
+
+
+class test_oseba(unittest.TestCase):
+
+	def test_starost(self):
 		clan = db_entities.init_oseba(rojstro_delta_days=12 * 365 + 6 * 31)
 		self.assertLess(clan.starost - 12.5, 0.01)
 
-	def test_clan_mladoletnik(self):
+	def test_mladoletnik(self):
 		clan1 = db_entities.init_oseba(rojstro_delta_days=18 * 365 + 1 * 30)
 		clan2 = db_entities.init_oseba(rojstro_delta_days=18 * 365 - 1 * 30)
 		self.assertFalse(clan1.mladoletnik)
 		self.assertTrue(clan2.mladoletnik)
 
-	def test_clan_vpisan(self):
+	def test_vpisan(self):
 		clan = db_entities.init_oseba(vpisi=[], izpisi=[])
 		self.assertFalse(clan.vpisan)
 		clan.vpisi.append(datetime.utcnow() + timedelta(days=5))
