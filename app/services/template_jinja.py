4bf05a840d1bc6c54f2d271f2c34ae75c9da7115
urosjarc <jar.fmf@gmail.com>
2022-12-22 00:13:21 +0100

    Fix errors in tests.

--- app/services/template_jinja.py
+++ app/services/template_jinja.py
@@ -27,7 +27,7 @@ class TemplateJinja(TemplateService):
 		self.__init()
 
 	def __init(self):
-		for ele, val in CONST.__dict__.prices():
+		for ele, val in CONST.__dict__.items():
 			if isinstance(val, str | float | int):
 				self.kwargs[f'CONST_{ele}'] = val
 
--- test/01_domain_tests/test_arhitektura_kluba.py
+++ test/01_domain_tests/test_arhitektura_kluba.py
@@ -12,7 +12,7 @@ class test_tip_osebe(unittest.TestCase):
 		dovoljenja = TipOsebe.dovoljenja()
 		self.assertGreater(len(values), 0)
 		self.assertEqual(len(dovoljenja.values()), len(dovoljenja.keys()))
-		for sk, sv in dovoljenja.prices():
+		for sk, sv in dovoljenja.items():
 			self.assertIn(sk, sv)
 
 
